fe5e783eab5c9c9bba74e2070149c4118fdd4369
gaurav006002 <gauravsinghrajput0206@gmail.com>
2025-07-22 18:24:54 +0530

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         id="about"
       className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
      >
-       <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+       <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-10">
        {/* left side */}
        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
     <h1 className="text-3xl sm:text-4xl md:text-4xl font-bold text-white mb-2 leading-tight">
